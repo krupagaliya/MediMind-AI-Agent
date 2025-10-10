74593d0812ff0e918a533c119d6b77dfb57dabfc
Krupa Galiya <krupagaliya@gmail.com>
2025-10-10 23:14:16 +0530

    Add the LoopAgent for demo and update to 2.5 flash

--- healthcare_agents/agent.py
+++ healthcare_agents/agent.py
@@ -8,7 +8,7 @@ Coordinates between three specialized sub-agents:
 
 
 from dotenv import load_dotenv
-from google.adk.agents import Agent
+from google.adk.agents import LlmAgent
 from google.adk.tools.agent_tool import AgentTool
 
 # Load environment variables from .env file
@@ -27,11 +27,12 @@ symptom_analyzer_tool = AgentTool(agent=symptom_agent)
 hospital_finder_tool = AgentTool(agent=hospital_finder_agent)
 home_remedies_tool = AgentTool(agent=home_remedies_agent)
 
+# Basic LLM Agent
 # Create the main coordinator agent
-root_agent = Agent(
+root_agent = LlmAgent(
     name="healthcare_coordinator",
-    model="gemini-2.0-flash",
-    # model="gemini-2.0-flash-live-001",
+    model="gemini-2.5-flash",
+    # model="gemini-2.0-flash-live-001", # Enable for voice mode.
     description=(
         "Main healthcare coordinator that manages three specialized sub-agents: "
         "a symptom analyzer for health assessments, a hospital finder for location services, "
@@ -43,4 +44,20 @@ root_agent = Agent(
         hospital_finder_tool,
         home_remedies_tool
     ],
-) 
+)
+
+
+# Loop Agent which will run the sub-agents in a loop (for coordination or multi-step reasoning).
+# from google.adk.agents import LoopAgent
+#
+# root_agent = LoopAgent(
+#     name='healthcare_coordinator',
+#     description=(
+#         "Main healthcare coordinator that manages three specialized sub-agents: "
+#                 "a symptom analyzer for health assessments, a hospital finder for location services, "
+#                 "and a home remedies advisor for natural treatments of light symptoms."
+#     ),
+#     max_iterations=2,
+#     sub_agents=[symptom_agent, home_remedies_agent, hospital_finder_agent],
+# )
+
